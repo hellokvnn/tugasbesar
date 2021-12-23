1318d94cccf153abcfe88f467a244c508628acbb
hellokvnn <herconstellation@gmail.com>
2021-12-23 22:32:57 +0700

    ubah tampilan index

--- resources/views/masakan/index.blade.php
+++ resources/views/masakan/index.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
 <br>
+<center>
 <h3>Data Masakan</h3>
-
+<br>
     <a href="" class="btn btn-success">Tambah Data</a>
+</center>
+<br>
 
     @if ($errors->any())
     <div class="alert alert-danger">
@@ -17,11 +20,11 @@
     @endif
 
     <table class="table table-bordered">
-        <thead class="thead-dark" align="center">
+        <thead class="thead-light" align="center">
             <tr>
                 <th>No</th>
                 <th>Nama</th>
-                <th>Jumlah Mahasiswa</th>
+                <th>Jumlah Masakan</th>
                 <th colspan=2>Aksi</th>
             </tr>
         </thead>
